fc6cba38a253517de4db5e53a1d9ccabe3139b33
oliverlee96 <oliverleej@hotmail.com>
2022-12-09 16:47:56 +0000

    Dynamically display player names on turn changes

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 //responsible for initialising the app and storing intital variables
 
 let editedPlayer = 0;
+let activePlayer = 0; // changes from 0 to 1 after each turn, so you can access this index in the array
 
 const players = [
     {
@@ -13,7 +14,6 @@ const players = [
     },
 ];
 
-
 //overlay and backdrop elements
 const configOverlay = document.getElementById('config-overlay');
 const backdropElement = document.getElementById('backdrop');
@@ -25,6 +25,12 @@ const configErrorText = document.getElementById('config-errors');
 const editPlayerOneButtonElement = document.getElementById('edit-player-one-button');
 const editPlayerTwoButtonElement = document.getElementById('edit-player-two-button');
 const overlayCancelButton = document.getElementById('cancel-config-button');
+const startNewGameButton = document.getElementById('start-new-game-button');
+const gameBoardElements = document.querySelectorAll('#game-board li');
+
+//game board
+const gameBoard = document.getElementById('active-game');
+const activePlayerName = document.getElementById('active-player-name');
 
 //event listeners for the buttons to edit player info
 editPlayerOneButtonElement.addEventListener('click', openPlayerConfig);
@@ -32,4 +38,17 @@ editPlayerTwoButtonElement.addEventListener('click', openPlayerConfig);
 overlayCancelButton.addEventListener('click', closePlayerConfig);
 backdropElement.addEventListener('click', closePlayerConfig);
 
-form.addEventListener('submit', savePlayerConfig) //submit is a built in browser event like 'click'
+form.addEventListener('submit', savePlayerConfig); //submit is a built in browser event like 'click'
+
+//starting a new game
+startNewGameButton.addEventListener('click', startNewGame);
+
+//adds event listeners for all gameboard list items
+for (const gameBoardElement of gameBoardElements) {
+    gameBoardElement.addEventListener('click', selectGameField);
+};
+
+
+
+
+
--- scripts/game.js
+++ scripts/game.js
@@ -1,1 +1,28 @@
 //responsible for the game logic
+
+//When you start a new game, checks if both player names are added and displays game board
+function startNewGame() {
+    if (players[0].name === '' || players[1].name === '') {
+        alert('Please set the player names for both players!') //TODO: Create custom modal for this warning. Specify which player name isn't set.
+
+        return; //stops executing function 
+    };
+    activePlayerName.textContent = players[activePlayer].name;
+    gameBoard.style.display = 'block';
+};
+
+function switchPlayer() {
+    if (activePlayer === 0) {
+        activePlayer = 1;
+    } else {
+        activePlayer = 0;
+    }
+    activePlayerName.textContent = players[activePlayer].name;
+
+};
+
+function selectGameField(event) {
+    event.target.textContent = players[activePlayer].symbol; // event.target gets access to the field in which the event occurred, and the [activePlayer] accesses that index number. So players[0], and you can just change the value to access player 2
+    event.target.classList.add('disabled'); //so it looks like you can't click again once clicked.
+    switchPlayer();
+};
